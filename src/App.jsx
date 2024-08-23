ca3c3bb6a536909e884584928087342a03a51e27
Peheliya <hwpeheliya@gmail.com>
2024-08-23 22:08:01 +0530

    Add some other routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import Home from "./pages/Home"
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import PrivateLayout from "./components/layout/PrivateLayout";
+import CategoryList from "./pages/category/CategoryList";
+import PostList from "./pages/post/PostList";
+import Profile from "./pages/Profile";
+import Setting from "./pages/Setting";
+import PublicLayout from "./components/layout/PublicLayout";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
 
 function App() {
-  return <Home/>;
+  return (
+    <Routes>
+      <Route element={<PrivateLayout />} >
+        <Route path="/" element={<Home/>} />
+        <Route path="categories" element={<CategoryList/>} />
+        <Route path="posts" element={<PostList/>} />
+        <Route path="profile" element={<Profile/>} />
+        <Route path="setting" element={<Setting/>} />
+      </Route>
+      <Route element={<PublicLayout />}>
+        <Route path="signup" element={<Signup/>} />
+        <Route path="login" element={<Login/>} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
